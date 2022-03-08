245259b5687377b5c0c2e13d04a1ee712d8e5c9a
JULNAVAR-T490\julnavar <julieta.navarro@oracle.com>
2022-03-07 19:16:57 -0600

    add slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,5 +1,22 @@
 import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { Carousel } from 'react-responsive-carousel';
+import { useFeaturedBanners } from "../../utils/hooks/useFeaturedBanners"
 
 export default function Slider() {
-  return <div>Slider</div>;
+  const { data, isLoading } = useFeaturedBanners();
+
+  if (isLoading) {
+    return <div>{"..."}</div>;
+  }
+  return (
+    <Carousel>
+      {data.results.map((banner) => (
+        <div key={banner.id}>
+            <img alt={banner.data.alt} src={banner.data.main_image.url}/>
+            <p className="legend">{banner.data.title}</p>
+        </div>
+      ))}
+    </Carousel>
+  );
 }
